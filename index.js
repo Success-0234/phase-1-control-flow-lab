2a1acf49826a36a0380725c590b799d309f8872a
David <Muchahi 853@gmail.com>
2024-06-25 10:47:53 +0300

    Finished assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+
+  if(feet<=400){
+    return "This one is on me!";
+  }else if(feet<2000){
+    return "That will be twenty bucks.";
+  }else if(feet<=2500){
+    return "I will gladly take your thirty bucks.";
+  }else{
+    return "No can do.";
+  }
+}
+console.log(scuberGreetingForFeet(2600));
+
+function ternaryCheckCity(City){
   // Write your code here!
+  return (City==="NYC") ? 'Ok, sounds good.': 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
-}
+  switch(Tip){
+    case "generous":
+      return "Thank you so much.";
+      case "not as generous":
+        return "Thank you.";
+        default:
+          return "Bye.";
+      
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  }
+  
+     
+  
 }
+console.log(switchOnCharmFromTip("tip is generous"))
